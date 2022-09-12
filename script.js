bf1c078bf82fc2242841ad4d08870d521632d076
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-09-12 21:25:54 +0100

    completed the logic for the game except for the win checker next step is to do the win checker so far everything is working there might be several places where global code can be reduced even more

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
-const button = document.querySelector(".playerCreator");
+const button = document.querySelector(".gameStarter");
+const playerNameInputField = document.querySelector(".playerName");
+const createPlayer = document.querySelector(".createPlayer");
+
+playerNameInputField.value = "Zeba";
+
 let zaidejas;
-button.addEventListener("click", () => {
-    console.log("click")
-    zaidejas = player1("dzimas");
+
+button.addEventListener("click", () => { 
+    
 })
 
+const player1 = (() => { 
+    console.log("player created");  
+    const move = (event) => {
+        event.srcElement.innerHTML = "&#9829";
+    }
+    return {move};
+ })();
+
+ const player2 = (() =>{
+    console.log("player2 created");
+    const move = (event) => {
+            event.srcElement.innerHTML = "&#9827";        
+    }
+    return {move};
+ })();
+
 const game = (() => {  
-    let counter = 0; 
+    let counter = 1; 
     const board = document.querySelectorAll(".gameBoardSquare");
     board.forEach(square => {
-        square.addEventListener("click", () => {            
-            if(counter % 2 == 0){player1()}
-            else{zaidejas.zeba()};
+        square.addEventListener("click", (e) => {
+            if(counter < 10 && e.srcElement.innerHTML == ""){  
+                console.log("Square clicked");          
+                if(counter % 2 == 0){player2.move(e); game.counterFunction()}
+                else{player1.move(e); game.counterFunction()};
+            }
+            else{console.log("invalid move");}
+
         });
-    });  
+    }); 
+    let counterFunction = () => {
+        console.log(counter);
+        counter++;
+        if(counter > 9){
+            winChecker();
+        }
+    }
+    let winChecker = () => {
+        console.log("somebody wins");
+    } 
 
-    return {board, counter};
+    return {board, counterFunction};
  })();
 
- const player1 = (playerName) => { 
-    console.log(playerName);   
-    const zeba = () => console.log(playerName);
-    return {zeba};
- }
 
- const computer = (() =>{
-    const zeba = () => console.log("zeba2");
-    console.log("computer");
-    return {zeba};
- })();
+
+
 
 
 
