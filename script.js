fa30e7385691dc516246722baf5258311a2c85fd
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-09-15 20:10:39 +0100

    added the option to choose player 1s name will need to do it for player 2 as well, added some styling changed the divs from being created to being displayed next step is to make the game possible for 1 player and for the computer to play as player 2

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const button = document.querySelector(".gameStarter");
-const playerNameInputField = document.querySelector(".playerName");
-const createPlayer = document.querySelector(".createPlayer");
-const bodyElement = document.querySelector("body");
-
-
+const winningScenarios = (() => {
+    const win = (winningPlayer) => {
+        const winDisplay = document.querySelector(".winningDisplay");
+        const winningText = document.querySelector(".winningText");
+        const restartButton = document.querySelector(".restartButton");
+        winningText.innerHTML = winningPlayer;
+        restartButton.addEventListener("click", () => {
+            winDisplay.classList.remove("active");            
+        })
+        setTimeout(() => {
+           winDisplay.classList.add("active");
+       }, 1)
+    }
+    return {win};
+})();
 
 const player1 = (() => { 
     
@@ -12,26 +21,7 @@ const player1 = (() => {
         game.movesArray[event.target.dataset.index] = 0;
         game.counterFunction();
     }
-    const win = () => {
-        console.log("player1 wins");
-       // console.log(game.movesArray, "moves array");
-        const winDisplay = document.createElement("div");
-        const winningText = document.createElement("h2");
-        const restartButton = document.createElement("button");
-        restartButton.innerHTML = "Restart Game";
-        winningText.innerHTML = "Player 1 Wins!!!";
-        winDisplay.classList.add("winningDisplay");
-        winDisplay.appendChild(winningText);
-        winDisplay.appendChild(restartButton);
-        bodyElement.appendChild(winDisplay);
-        restartButton.addEventListener("click", () => {
-            winDisplay.remove();
-        })
-        setTimeout(() => {
-            winDisplay.classList.add("active");
-        }, 1)
-    }
-    return {move, win};
+    return {move};
  })();
 
  const player2 = (() =>{
@@ -41,110 +31,71 @@ const player1 = (() => {
             game.movesArray[event.target.dataset.index] = 1;   
             game.counterFunction();   
     }
-    const win = () => {
-       // console.log("player2 wins");
-       // console.log(game.movesArray, "moves array");
-        const winDisplay = document.createElement("div");
-        const winningText = document.createElement("h2");
-        const restartButton = document.createElement("button");
-        restartButton.innerHTML = "Restart Game";
-        winningText.innerHTML = "Player 2 Wins!!!";
-        winDisplay.classList.add("winningDisplay");
-        winDisplay.appendChild(restartButton);
-        winDisplay.appendChild(winningText);
-        bodyElement.appendChild(winDisplay);
-
-        restartButton.addEventListener("click", () => {
-            winDisplay.remove();            
-        })
-        setTimeout(() => {
-            winDisplay.classList.add("active");
-        }, 1)
-
-    }
-    return {move, win};
+    return {move};
  })();
 
 const game = (() => {
+    let player1Name;
+    let player2Name = "Player 2";
     let movesArray = [];  
-    let counter = 1; 
+    let counter = 1;
+
+    const playerNameInputField = document.querySelector(".playerName");
     const board = document.querySelectorAll(".gameBoardSquare");
+    const button = document.querySelector(".gameStarter");
+    const createPlayer = document.querySelector(".createPlayer");
+
+    button.addEventListener("click", () => {
+        createPlayer.classList.remove("active");
+        player1Name = playerNameInputField.value;
+    })
 
     board.forEach(square => {
         square.addEventListener("click", (e) => {
             if(counter < 10 && e.srcElement.innerHTML == ""){ 
-                console.log(counter, "counter");
+
                 if(counter % 2 == 0) player2.move(e);
                 else player1.move(e); 
-                game.winChecker(); 
+
+                winChecker(); 
             }
             else console.log("invalid move");
-
         });
     }); 
     let counterFunction = () => {
-
         counter++;
-
         if(counter > 9){
             counter = 1;
         }
     }
-    let draw = () => {
-        console.log("draw");
-        // console.log(game.movesArray, "moves array");
-         const winDisplay = document.querySelector(".winningDisplay");
-         const winningText = document.querySelector(".winningText");
-         const restartButton = document.querySelector(".restartButton");
-         winningText.innerHTML = "Draw";
-
-         restartButton.addEventListener("click", () => {
-             winDisplay.classList.remove("active");            
-         })
-         setTimeout(() => {
-            winDisplay.classList.add("active");
-        }, 1)
-       }
 
     let winChecker = () => {
-        console.log("checking for wins")
-        let checkCounter = 0;
-        let player1rowCounter = 0;
-        let player2rowCounter = 0;
-        let player1columnCounter = 0;
-        let player2columnCounter = 0;
-
+        let checkCounter = player1rowCounter = player1columnCounter = player2rowCounter = player2columnCounter = 0;
         let i = j = 0;
         let k = 3;
 
         while(checkCounter < 3){
-            console.log(j, "j");
+
             for(; i < k; i++)
             {
-
                 if(game.movesArray[i] == 0) player1rowCounter++;
                 else if(game.movesArray[i] == 1) player2rowCounter++;
-
             }
 
             for(; j < 9; j += 3)
             {
-
                 if(game.movesArray[j] == 0) player1columnCounter++;
                 if(game.movesArray[j] == 1) player2columnCounter++;
-
             }
-           // console.log(player2rowCounter, "2rowCounter");
-            //console.log(player2columnCounter, "1columnCounter");
+
             if(player1rowCounter == 3 || player1columnCounter == 3) 
-            {   
-                //console.log(player2columnCounter, "1columnCounterinsideif");
+            { 
                 board.forEach(square => {
                     square.innerHTML = "";
                 })
                 counter = 1;
                 game.movesArray = [];
-                 return player1.win();
+                return winningScenarios.win(`${player1Name} Wins!!!`);
             }
 
             else if(player2rowCounter == 3 || player2columnCounter == 3)
@@ -154,32 +105,27 @@ const game = (() => {
                 }) 
                 counter = 1;
                 game.movesArray = [];     
-                return player2.win();   
+                return winningScenarios.win(`${player2Name} Wins!!!`);   
             }
+
             else if(counter == 1 && game.movesArray != []){
                 board.forEach(square => {
                     square.innerHTML = "";
                 }) 
                 counter = 1;
                 game.movesArray = [];
-                return draw();
+                return winningScenarios.win("Draw");
             }
             checkCounter++;
             k += 3;
-            //console.log(player1columnCounter, player1rowCounter);
-            player1rowCounter = 0;
-            player2rowCounter = 0;
-            player1columnCounter = 0;
-            player2columnCounter = 0;
+            player1rowCounter = player1columnCounter = player2rowCounter = player2columnCounter = 0;
 
             j = checkCounter;
         }
 
     
     } 
-
-
-    return {counterFunction, movesArray, winChecker};
+    return {counterFunction, movesArray};
  })();
 
 
