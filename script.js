08b7d56c1fd331e112c3c9f83ee23693f7194fdb
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-09-18 19:38:00 +0100

    allowed for the choice between pvp and pvc added a few extra screens to enter the game

--- script.js
+++ script.js
@@ -10,16 +10,24 @@ const winningScenarios = (() => {
         setTimeout(() => {
            winDisplay.classList.add("active");
        }, 1)
+       console.log("win");
     }
     return {win};
 })();
 
 const player1 = (() => { 
     
-    const move = (event) => {
+    const move = (event, player2Name) => {
+
         event.srcElement.innerHTML = "&#9829";
         game.movesArray[event.target.dataset.index] = 0;
         game.counterFunction();
+        game.winChecker();
+        console.log(game.movesArray);
+        if(player2Name == "computer" && game.movesArray.length != 0){
+            game.computerPlay(event);
+            game.winChecker();
+        }
     }
     return {move};
  })();
@@ -29,25 +37,68 @@ const player1 = (() => {
     const move = (event) => {
             event.srcElement.innerHTML = "&#9827";  
             game.movesArray[event.target.dataset.index] = 1;   
-            game.counterFunction();   
+            game.counterFunction(); 
+            game.winChecker();  
     }
     return {move};
  })();
 
 const game = (() => {
-    let player1Name;
-    let player2Name = "Player 2";
+    let createdPlayers = 0;
+    let player1Name, player2Name;
     let movesArray = [];  
     let counter = 1;
 
-    const playerNameInputField = document.querySelector(".playerName");
+    const chooseOpponentScreen = document.querySelector(".playerOrComputerWrap");
+    const computerChoice = document.querySelector(".computer");
+    const playerChoice = document.querySelector(".player");
+    const playerNames = document.querySelector(".playerName");
+    const playerNameInputField = document.querySelector(".playerNameInput");
     const board = document.querySelectorAll(".gameBoardSquare");
     const button = document.querySelector(".gameStarter");
     const createPlayer = document.querySelector(".createPlayer");
+    const gameBoard = document.querySelector(".gameBoardWrapWrap");
+    const background = document.querySelector(".background");
 
-    button.addEventListener("click", () => {
-        createPlayer.classList.remove("active");
-        player1Name = playerNameInputField.value;
+    playerNames.innerHTML = "Player 1 Name";
+    
+    const buttonFunction = (playerOrComputer) => {
+        console.log("button function");
+        button.addEventListener("click", () => {
+        
+        if(playerOrComputer == "player"){
+            player1Name = playerNameInputField.value;
+            playerNames.innerHTML = "Player 2 Name";
+            playerNameInputField.value = "";
+            if(createdPlayers == 1)
+            {
+                createPlayer.classList.remove("active");
+                player2Name = playerNameInputField.value;  
+                gameBoard.classList.add("active");
+                background.classList.remove("active");
+            }
+            createdPlayers++;
+        }
+        else if(playerOrComputer == "computer"){
+
+            player1Name = playerNameInputField.value;
+            player2Name = "computer";
+            createPlayer.classList.remove("active");
+            gameBoard.classList.add("active");
+            background.classList.remove("active");
+        }
+        })
+    }
+
+    playerChoice.addEventListener("click", () => {
+        chooseOpponentScreen.classList.remove("active");
+        createPlayer.classList.add("active");
+        buttonFunction("player");
+    })
+    computerChoice.addEventListener("click", () => {
+        chooseOpponentScreen.classList.remove("active");
+        createPlayer.classList.add("active");
+        buttonFunction("computer");
     })
 
     board.forEach(square => {
@@ -55,9 +106,8 @@ const game = (() => {
             if(counter < 10 && e.srcElement.innerHTML == ""){ 
 
                 if(counter % 2 == 0) player2.move(e);
-                else player1.move(e); 
-
-                winChecker(); 
+                else player1.move(e, player2Name); 
+            
             }
             else console.log("invalid move");
         });
@@ -70,6 +120,7 @@ const game = (() => {
     }
 
     let winChecker = () => {
+        console.log("win Check");
         let checkCounter = player1rowCounter = player1columnCounter = player2rowCounter = player2columnCounter = 0;
         let i = j = 0;
         let k = 3;
@@ -87,14 +138,16 @@ const game = (() => {
                 if(game.movesArray[j] == 0) player1columnCounter++;
                 if(game.movesArray[j] == 1) player2columnCounter++;
             }
-
+            console.log(player2rowCounter, player2columnCounter);
             if(player1rowCounter == 3 || player1columnCounter == 3) 
             { 
                 board.forEach(square => {
                     square.innerHTML = "";
                 })
                 counter = 1;
+                console.log(game.movesArray);
                 game.movesArray = [];
+                
                 return winningScenarios.win(`${player1Name} Wins!!!`);
             }
 
@@ -104,6 +157,7 @@ const game = (() => {
                     square.innerHTML = "";
                 }) 
                 counter = 1;
+                console.log(game.movesArray);
                 game.movesArray = [];     
                 return winningScenarios.win(`${player2Name} Wins!!!`);   
             }
@@ -125,7 +179,24 @@ const game = (() => {
 
     
     } 
-    return {counterFunction, movesArray};
+    let computerPlay = (event) => {
+        console.log("computer play");
+        let i = 0;
+
+        while (i < 1){
+            
+            let randomNumber = Math.floor(Math.random() * 9);
+            if(board[randomNumber].innerHTML == "")
+            {
+            board[randomNumber].innerHTML = "&#9827";  
+            game.movesArray[randomNumber] = 1;
+            console.log(movesArray, "moves array inside computerplay function");
+            counterFunction(); 
+            i++;
+            }
+        }
+    }
+    return {counterFunction, movesArray, player2Name, computerPlay, winChecker};
  })();
 
 
